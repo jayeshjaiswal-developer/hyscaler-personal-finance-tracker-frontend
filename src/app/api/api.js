32c3aac924a12cf36b71304347c0072c45c0825a
jayeshdeveloper <jayesh.developer546@gmail.com>
2026-04-20 03:10:52 +0530

    backend url modified

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,4 +1,6 @@
-exports.backendBaseUrl = 'http://localhost:8081';
+// exports.backendBaseUrl = 'http://localhost:8081';
+exports.backendBaseUrl = 'http://43.204.56.43:8081';
+
 
 
 // exports.backendBaseUrl = 'https://server-jaiswal-retail-production.up.railway.app';
